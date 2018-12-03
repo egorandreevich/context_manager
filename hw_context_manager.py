174a126d52a31daeba868e93df055c53be9c52fd
egorandreevich <45241457+egorandreevich@users.noreply.github.com>
2018-12-03 21:57:43 +0000

    Update hw_context_manager.py

--- hw_context_manager.py
+++ hw_context_manager.py
@@ -2,13 +2,20 @@ import time
 
 start_time = time.time()
 print('Время начала работы программы {}'.format(time.ctime(start_time)))
+
 with open('test.txt', 'wt', encoding='utf-8') as f:
-    num = int(input())
-    line = str('1/' + str(num) + '=' + str(1/num))
-    print(line)
-    f.write(line)
+    def vodka_liters():
+        print('Делим количество водки на человека.\nВведите, сколько у вас есть литров:')
+        num = float(input())
+        print('Сколько вас человек?')
+        people = int(input())
+        vodka_per_person = num / people
+        return ('Каждому по: {} литру/литра'.format(vodka_per_person))
+    try:
+        f.write(vodka_liters())
+    except ValueError:
+        print('Значение не является числом')
 
 
-# main()
 print('Время окончания работы программы {}'.format(time.ctime(time.time())))
 print("Работа программы заняла %s секунд" % (time.time() - start_time))
